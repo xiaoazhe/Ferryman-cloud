9f0060dc45825b2d5313cc93f8053407124f3b91
摆渡人 <1540035958@qq.com>
2021-06-24 13:28:29 +0800

    update

--- ferry-admin/src/main/java/com/ferry/FerryAdminApplication.java
+++ ferry-admin/src/main/java/com/ferry/FerryAdminApplication.java
@@ -4,6 +4,7 @@ import com.ferry.common.utils.IdWorker;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 
