859e3b6d5b1d2bdc683ca984478f00846c2ddbbf
摆渡人 <1540035958@qq.com>
2021-07-13 20:52:12 +0800

    update dept parentname

--- ferry-admin/src/main/java/com/ferry/admin/controller/MenuController.java
+++ ferry-admin/src/main/java/com/ferry/admin/controller/MenuController.java
@@ -8,10 +8,12 @@ import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 
 /**
  * 菜单管理
+ *
  * @Author: 摆渡人
  * @Date: 2021/4/26
  */
@@ -25,28 +27,28 @@ public class MenuController {
 
     @ApiOperation(value = "添加菜单")
     @PreAuthorize("hasAuthority('sys:menu:add') AND hasAuthority('sys:menu:edit')")
-    @PostMapping(value="/save")
+    @PostMapping(value = "/save")
     public Result save(@RequestBody SysMenu record) {
         return Result.ok(sysMenuService.save(record));
     }
 
     @ApiOperation(value = "删除菜单")
     @PreAuthorize("hasAuthority('sys:menu:delete')")
-    @PostMapping(value="/delete")
-    public Result delete(@RequestBody List <SysMenu> records) {
+    @PostMapping(value = "/delete")
+    public Result delete(@RequestBody List<SysMenu> records) {
         return Result.ok(sysMenuService.delete(records));
     }
 
     @ApiOperation(value = "根据登录人员获取左侧菜单")
     @PreAuthorize("hasAuthority('sys:menu:view')")
-    @GetMapping(value="/findNavTree")
+    @GetMapping(value = "/findNavTree")
     public Result findNavTree(@RequestParam String userName) {
         return Result.ok(sysMenuService.findTree(userName, 1));
     }
 
     @ApiOperation(value = "获取菜单")
     @PreAuthorize("hasAuthority('sys:menu:view')")
-    @GetMapping(value="/findMenuTree")
+    @GetMapping(value = "/findMenuTree")
     public Result findMenuTree() {
         return Result.ok(sysMenuService.findTree(null, 0));
     }
--- ferry-admin/src/main/java/com/ferry/blog/service/impl/MusicServiceImpl.java
+++ ferry-admin/src/main/java/com/ferry/blog/service/impl/MusicServiceImpl.java
@@ -65,7 +65,7 @@ public class MusicServiceImpl extends ServiceImpl <BlMusicMapper, BlMusic> imple
         QueryWrapper<BlMusic> queryWrapper = new QueryWrapper();
         queryWrapper.like(!StringUtils.isBlank(pageRequest.getName()), BlMusic.COL_NAME, pageRequest.getName());
         queryWrapper.like(!StringUtils.isBlank(pageRequest.getName()), BlMusic.COL_ARTIST, pageRequest.getName());
-        queryWrapper.eq(pageRequest.getEnabled() > -1, BlMusic.COL_ENABLE, pageRequest.getEnabled());
+        queryWrapper.eq(pageRequest.getEnabled() > 0, BlMusic.COL_ENABLE, pageRequest.getEnabled());
         Page<BlMusic> typePage = musicMapper.selectPage(page, queryWrapper);
         PageResult pageResult = new PageResult(typePage);
         return pageResult;
