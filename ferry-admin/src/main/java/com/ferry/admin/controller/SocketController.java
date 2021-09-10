f2753d33a76b02bb979d84ba8f62619339b8510d
摆渡人 <1540035958@qq.com>
2021-09-10 17:30:31 +0800

    update

---
+++ ferry-admin/src/main/java/com/ferry/admin/controller/SocketController.java
@@ -0,0 +1,10 @@
+package com.ferry.admin.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/socket")
+public class SocketController {
+    
+}
